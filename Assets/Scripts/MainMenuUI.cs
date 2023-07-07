f9b40509c1da0805b9483208ccbdfeaf6c637373
lekirschbaum <lekirschbaum@outlook.com>
2023-07-07 17:25:23 +0200

    ADDED: Quit Panel // CoreCraft Website Linking

--- Assets/Scripts/MainMenuUI.cs
+++ Assets/Scripts/MainMenuUI.cs
@@ -9,10 +9,22 @@ namespace JamCraft.GMTK2023.Code
         [Header("UI Buttons")] 
         [SerializeField] private Button _playButton;
         [SerializeField] private Button _quitButton;
+        [SerializeField] private Button _coreCraftButton;
+
+        [Header("Quit Panel")]
+        [SerializeField] private GameObject _quitPanel;
+        [SerializeField] private Button _yesButton;
+        [SerializeField] private Button _noButton;
 
         private void Awake()
         {
+            SetupUI();
             SetupUIButtons();
+        }
+
+        private void SetupUI()
+        {
+            _quitPanel.SetActive(false);
         }
 
         private void SetupUIButtons()
@@ -21,9 +33,30 @@ namespace JamCraft.GMTK2023.Code
             {
                 Loader.Load(Loader.Scene.game_scene);
             });
+
             _quitButton.onClick.AddListener(() =>
             {
+                _quitPanel.SetActive(true);
+            });
+
+            _coreCraftButton.onClick.AddListener(() =>
+            {
+                Application.OpenURL("https://www.corecraft-games.com");
+            });
+
+            _yesButton.onClick.AddListener(() =>
+            {
+                // TODO: Add Game Jam page. Application.OpenURL("url").
+#if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+#else
                 Application.Quit();
+#endif
+            });
+
+            _noButton.onClick.AddListener(() =>
+            {
+                _quitPanel.SetActive(false);
             });
         }
     }

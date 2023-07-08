afedf98edc7110ac3756ac57beb877d9266e2ce6
lekirschbaum <lekirschbaum@outlook.com>
2023-07-09 01:53:05 +0200

    UPDATED: Score UI Animations

--- Assets/Scripts/ScoreUI.cs
+++ Assets/Scripts/ScoreUI.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
+using DG.Tweening;
 
 namespace JamCraft.GMTK2023.Code
 {
@@ -16,6 +16,15 @@ namespace JamCraft.GMTK2023.Code
         [SerializeField] private GameObject _streakPrefab;
         [SerializeField] private List<GameObject> _streaks = new List<GameObject>();
 
+        private int _oldScore = 0;
+        private int _oldTemporaryScore = 0;
+
+        private int _newScore = 0;
+        private int _newTemporaryScore = 0;
+
+        private Tween _scoreTween;
+        private Tween _temporaryScoreTween;
+
         private void Start()
         {
             EventManager.Instance.PointsAddedEvent.AddListener(AddScore);
@@ -23,18 +32,34 @@ namespace JamCraft.GMTK2023.Code
             EventManager.Instance.TempPointsEvent.AddListener(AddTemporaryScore);
             EventManager.Instance.StreakEndEvent.AddListener(StreakEndEvent);
             EventManager.Instance.MissedMultiplyEvent.AddListener(MissedMultiplierEvent);
-
+            
             for (int i = 0; i < 3; i++)
             {
                 _streaks.Add(Instantiate(_streakPrefab, _streakHolder.transform));
             }
+
+            _scoreTween.SetAutoKill(false).Pause();
+            _temporaryScoreTween.SetAutoKill(false).Pause();
         }
         
         private void AddScore(int value)
         {
-            _scoreText.text = "Score: " + value.ToString();
+            _oldScore = _newScore;
+            _newScore = value;
+
+            _scoreTween = _scoreText.DOCounter(_oldScore, _newScore, 2f, false, null);
+            //_scoreText.DOCounter(_oldScore, _newScore, 2f, false, null);
+
+            if (_scoreTween.playedOnce)
+            {
+                _scoreTween.Restart();
+            }
+            else
+            {
+                _scoreTween.Play();
+            }
         }
-        
+
         public void SetMultiplier(float value)
         {
             _multiplierText.text = "Multiplier: " + value.ToString() + "x";
@@ -47,7 +72,20 @@ namespace JamCraft.GMTK2023.Code
 
         public void AddTemporaryScore(int value)
         {
-            _temporaryScoreText.text = "Temporary Score: " + value.ToString();
+            _oldTemporaryScore = _newTemporaryScore;
+            _newTemporaryScore = value;
+
+            _temporaryScoreTween = _temporaryScoreText.DOCounter(_oldTemporaryScore, _newTemporaryScore, 2f, false, null);
+            //_temporaryScoreText.DOCounter(_oldTemporaryScore, _newTemporaryScore, 2f, false, null);
+
+            if (_temporaryScoreTween.playedOnce)
+            {
+                _temporaryScoreTween.Restart();
+            }
+            else
+            {
+                _temporaryScoreTween.Play();
+            }
         }
 
         private void StreakEndEvent()
@@ -57,6 +95,8 @@ namespace JamCraft.GMTK2023.Code
 
         private void MissedMultiplierEvent(int value)
         {
+            Debug.Log("Missed a multiplier!");
+
             for (int i = 0; i < value; i++)
             {
                 _streaks[i].SetActive(false);

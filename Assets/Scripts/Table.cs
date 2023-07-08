ca14118d16187c8b0b5f2e59aced7dd8d26b6a07
JulianBrandt123 <jubrandt@stud.mediadesign.de>
2023-07-08 19:24:03 +0200

    UPDATED Turning during pause

--- Assets/Scripts/Table.cs
+++ Assets/Scripts/Table.cs
@@ -12,14 +12,14 @@ public class Table : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A) && Playermanager.Instance.CanTurn)
+        if (Input.GetKeyDown(KeyCode.A) /*&& Playermanager.Instance.CanTurn*/)
         {
             _rotation -= 90f;
             _playTable.transform.DORotate(_rotation * Vector3.up, _rotationTime);
             EventManager.Instance.PlayAudio.Invoke(4, 0);
             Playermanager.Instance.TurnRight();
         }
-        if (Input.GetKeyDown(KeyCode.D) && Playermanager.Instance.CanTurn)
+        if (Input.GetKeyDown(KeyCode.D) /*&& Playermanager.Instance.CanTurn*/)
         {
             _rotation += 90f;
             _playTable.transform.DORotate(_rotation * Vector3.up, _rotationTime);

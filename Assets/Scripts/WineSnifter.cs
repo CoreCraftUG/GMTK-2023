8d8d01e939d2eb2995360abcac60bf98f61fd61d
DESKTOP-C2QM312\Florian <florian.trippe@corecraft-games.com>
2023-07-09 15:55:29 +0200

    ADDED: wine

---
+++ Assets/Scripts/WineSnifter.cs
@@ -0,0 +1,23 @@
+using JamCraft.GMTK2023.Code;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class WineSnifter : MonoBehaviour
+{
+    Animator animator => GetComponent<Animator>();
+
+    void Update()
+    {
+        if ((Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.A)) && !GameStateManager.Instance.IsGamePaused && !GameStateManager.Instance.IsGameOver && Playermanager.Instance.CanTurn)
+        {
+            animator.SetBool("sway",true);
+        }
+    }
+
+    public void StopSwaying()
+    {
+        animator.SetBool("sway", false);
+    }
+}
